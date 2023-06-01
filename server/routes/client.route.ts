c997f9b371fe4a492e39fb39d7f6e1a3a1632ce8
Harsh Kumar <72347597+Harshkumar77@users.noreply.github.com>
2023-06-01 11:11:00 +0530

    added css and js support for render

--- server/routes/client.route.ts
+++ server/routes/client.route.ts
@@ -5,5 +5,10 @@ function sendClientCode(req: Request, res: Response) {
   res.sendFile("dist/index.html")
 }
 
+function sendAdditionalCode(req: Request, res: Response) {
+  res.sendFile(`dist/${req.params.file}`)
+}
+
 clientRouter.get("/", sendClientCode)
 clientRouter.get("/url/:id", sendClientCode)
+clientRouter.get("/assets/:file", sendAdditionalCode)
