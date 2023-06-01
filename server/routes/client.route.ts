c3cdf8d835939b658d8ea62a78d25443a7e7ac7c
Harsh Kumar <72347597+Harshkumar77@users.noreply.github.com>
2023-06-01 11:04:21 +0530

    updated index to dist

--- server/routes/client.route.ts
+++ server/routes/client.route.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express"
 export const clientRouter = express.Router()
 
 function sendClientCode(req: Request, res: Response) {
-  res.sendFile("build/index.html")
+  res.sendFile("dist/index.html")
 }
 
 clientRouter.get("/", sendClientCode)
